00319ce35fd0f49360887daaa7e8f039fb49ecf1
Anik mahmud <96568968+AnikMahmud78@users.noreply.github.com>
2026-09-11 10:10:26 +0600

    Mobile Poka-Yoke Input Masking Numerics 688BPTR-0725-A06

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'widgets/poka_yoke_numeric_556BPTR0725A04.dart';
+import 'widgets/poka_yoke_numeric_mask_688BPTR0725A06.dart';
 
 void main() {
-  runApp(const PokaYokeNumeric556BPTR0725A04App());
+  runApp(const PokaYokeNumericMask688BPTR0725A06App());
 }
 
-class PokaYokeNumeric556BPTR0725A04App extends StatelessWidget {
-  const PokaYokeNumeric556BPTR0725A04App({super.key});
+class PokaYokeNumericMask688BPTR0725A06App extends StatelessWidget {
+  const PokaYokeNumericMask688BPTR0725A06App({super.key});
 
   @override
   Widget build(BuildContext context) => MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: 'Poka-Yoke Numeric Forms',
+    title: 'Poka-Yoke Numeric Masking App',
     theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
-    home: const PokaYokeNumeric556BPTR0725A04(),
+    home: const PokaYokeNumericMask688BPTR0725A06(),
   );
 }
