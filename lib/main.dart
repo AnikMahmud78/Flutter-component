17369cac602a796f8c173b5a9a6efdbf32983b97
Anik mahmud <96568968+AnikMahmud78@users.noreply.github.com>
2026-09-11 10:11:18 +0600

    Isolated Critical Data Elements UI Form Layout 5561BPWSO-009-09

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'widgets/edge_regex_mask_5990BPTR0803A11.dart';
+import 'widgets/isolated_cde_form_5561BPWSO00909.dart';
 
 void main() {
-  runApp(const EdgeRegexMask5990BPTR0803A11App());
+  runApp(const IsolatedCdeForm5561BPWSO00909App());
 }
 
-class EdgeRegexMask5990BPTR0803A11App extends StatelessWidget {
-  const EdgeRegexMask5990BPTR0803A11App({super.key});
+class IsolatedCdeForm5561BPWSO00909App extends StatelessWidget {
+  const IsolatedCdeForm5561BPWSO00909App({super.key});
 
   @override
   Widget build(BuildContext context) => MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: 'Edge RegEx Mask App',
+    title: 'Isolated CDE Form App',
     theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
-    home: const EdgeRegexMask5990BPTR0803A11(),
+    home: const IsolatedCdeForm5561BPWSO00909(),
   );
 }
